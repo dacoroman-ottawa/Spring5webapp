d9f652fabc646188b06ae27320617ddccf9736b1
dacoroman-ottawa <62803075+dacoroman-ottawa@users.noreply.github.com>
2020-03-31 21:55:47 -0400

    Assignment: Add Publisher, PublisherRepository

---
+++ src/main/java/guru/springframework/spring5webapp/repositories/PublisherRepository.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package guru.springframework.spring5webapp.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import guru.springframework.spring5webapp.domain.Publisher;
+
+/**
+ * @author BNeagu
+ *
+ */
+
+
+public interface PublisherRepository extends CrudRepository<Publisher, Long>   {
+
+}
